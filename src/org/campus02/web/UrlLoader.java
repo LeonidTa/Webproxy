44dcf76c1b809911018adca78e43fba7ffbf9185
Leo <leonid.tahiraj@tutanota.com>
2022-05-28 09:35:50 +0200

    UrlLoader.java

---
+++ src/org/campus02/web/UrlLoader.java
@@ -0,0 +1,12 @@
+package org.campus02.web;
+
+import org.campus02.exception.UrlLoaderException;
+
+public class UrlLoader {
+
+    public static WebPage loadWebPage(String url) throws UrlLoaderException {
+
+
+        return null;
+    }
+}
