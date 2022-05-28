350f0e25d77fd5587b48523f5bd43486ddf41abf
Leo <leonid.tahiraj@tutanota.com>
2022-05-28 12:43:43 +0200

    server

--- src/org/campus02/web/Server.java
+++ src/org/campus02/web/Server.java
@@ -1,5 +1,7 @@
 package org.campus02.web;
 
+import org.campus02.exception.UrlLoaderException;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -7,14 +9,24 @@ import java.net.Socket;
 public class Server {
     public static void main(String[] args) {
 
-        try (ServerSocket serverSocket = new ServerSocket(5678);
-             Socket client = serverSocket.accept()){
+            try (ServerSocket serverSocket = new ServerSocket(5678);
+                 Socket client = serverSocket.accept()) {
+
+                while (true) {
+                    PageCache pageCache = new PageCache();
+                    pageCache.warmUp("C:\\Users\\Leo\\Desktop\\JAVA\\SS2022_FH\\fileIn.txt");
+                    WebProxy webProxy = new WebProxy(pageCache);
+
+                    ClientHandler clientHandler = new ClientHandler(client, webProxy);
+                    clientHandler.run();
+                }
 
 
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (UrlLoaderException e) {
+                e.printStackTrace();
+            }
 
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
