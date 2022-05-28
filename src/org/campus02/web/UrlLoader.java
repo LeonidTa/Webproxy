ce3739a62c0681dff3dbe6ed906d0ea7b5638487
Leo <leonid.tahiraj@tutanota.com>
2022-05-28 10:04:40 +0200

    UrlLoader.java implemented

--- src/org/campus02/web/UrlLoader.java
+++ src/org/campus02/web/UrlLoader.java
@@ -2,11 +2,30 @@ package org.campus02.web;
 
 import org.campus02.exception.UrlLoaderException;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class UrlLoader {
 
     public static WebPage loadWebPage(String url) throws UrlLoaderException {
 
+        try {
+            URL myUrl = new URL(url);
+            BufferedReader br = new BufferedReader(new InputStreamReader(myUrl.openStream()));
 
-        return null;
+            String totalContent = null;
+            String line;
+            while ((line = br.readLine()) != null) {
+                totalContent = totalContent + line;
+            }
+            System.out.println(totalContent);
+
+            return new WebPage(url, totalContent);
+
+        } catch (Exception e) {
+            throw new UrlLoaderException("cannot load URL ", e);
+        }
     }
 }
